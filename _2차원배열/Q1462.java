31a3129690db5e923a072ddcf37b4cea6876ed65
HyoJeong-Shin <kyn05298@naver.com>
2020-11-18 23:37:07 +0900

    11/18 java

---
+++ _2차원배열/Q1462.java
@@ -0,0 +1,33 @@
+package _2차원배열;
+
+import java.util.Scanner;
+
+public class Q1462 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.close();
+		
+		int[][] arr = new int[n][n];
+		
+		
+		
+		for(int i=0; i<n; i++) {
+			int k = i+1;
+			for(int j=0; j<n; j++) {
+				arr[i][j] = k;
+				k = k+n;
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
