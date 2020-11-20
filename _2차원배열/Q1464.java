44dc87db200bda8982918fca3c7d7080f581c018
HyoJeong-Shin <kyn05298@naver.com>
2020-11-20 23:11:54 +0900

    11/20 java

---
+++ _2차원배열/Q1464.java
@@ -0,0 +1,31 @@
+package _2차원배열;
+
+import java.util.Scanner;
+
+public class Q1464 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		int[][] arr = new int[n][m];
+		int data = n*m;
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				arr[i][j] = data;
+				data--;
+			}
+		}
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+}
